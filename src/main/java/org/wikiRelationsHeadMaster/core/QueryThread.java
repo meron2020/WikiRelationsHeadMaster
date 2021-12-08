084ef349b5f7ad2ec1a8d2815f359d1161cbe525
Yoav <yoav.meron11@gmail.com>
2021-12-08 15:59:38 +0200

    Changed QueryThread to use linksObject.

--- src/main/java/org/wikiRelationsHeadMaster/core/QueryThread.java
+++ src/main/java/org/wikiRelationsHeadMaster/core/QueryThread.java
@@ -1,27 +1,60 @@
 package org.wikiRelationsHeadMaster.core;
 
 import org.jsoup.nodes.Document;
+import org.wikiRelationsHeadMaster.core.Parsers.HtmlParser;
+import org.wikiRelationsHeadMaster.core.linksObjects.LinksObject;
+import org.wikiRelationsHeadMaster.core.Downloaders.PageDownloader;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class QueryThread {
+public class QueryThread extends Thread {
     public Integer id;
     public String query;
+    public LinksObject linksObject = new LinksObject();
+    public boolean readyToBeSent = false;
+    public boolean linksSent = false;
+    public boolean linksReceived = false;
 
     QueryThread(String query, Integer id) {
         this.query = query;
         this.id = id;
     }
 
-    public Document downloadQueryPage(String query) throws IOException {
+    public LinksObject getLinksObject() {
+        return linksObject;
+    }
+
+    public void setLinksObject(LinksObject linksObject) {
+        this.linksObject = linksObject;
+    }
+
+    public void setLinksSent(boolean linksSent) {
+        this.linksSent = linksSent;
+    }
+
+    public void setLinksReceived(boolean linksReceived) {
+        this.linksReceived = linksReceived;
+    }
+
+    public void setReadyToBeSent(boolean readyToBeSent) {
+        this.readyToBeSent = readyToBeSent;
+    }
+
+    public void initializeLinksObject(String query) throws IOException {
         String link = PageDownloader.getPageLinkFromQuery(query);
-        return PageDownloader.downloadPage(link);
+        Document queryDoc = PageDownloader.downloadPage(link);
+        this.linksObject.setLinks(HtmlParser.findAllWikiLinks(queryDoc));
+        this.linksObject.setOriginalLink(link);
+        this.linksObject.setId(this.id);
+        this.readyToBeSent = true;
     }
 
     public Object getLinksObjectFromHash(Hashtable<Integer, Object> hashtable) {
         return hashtable.get(this.id);
     }
 
+    public void start() {
+
+    }
 }
