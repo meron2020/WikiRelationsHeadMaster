11b7a5c95f2103997356f9115c09fd09f3dce593
Yoav <yoav.meron11@gmail.com>
2021-12-24 19:38:41 +0200

    Changed handler to handle all raw links related functionality and removed it from ApplicationManager

--- src/main/java/org/wikiRelationsHeadMaster/core/linkHandlers/RawLinksHandler.java
+++ src/main/java/org/wikiRelationsHeadMaster/core/linkHandlers/RawLinksHandler.java
@@ -1,5 +1,6 @@
 package org.wikiRelationsHeadMaster.core.linkHandlers;
 
+import org.wikiRelationsHeadMaster.core.Parsers.JsonParsingClass;
 import org.wikiRelationsHeadMaster.core.QueryThread;
 import org.wikiRelationsHeadMaster.core.linksObjects.LinksObject;
 import org.wikiRelationsHeadMaster.core.networking.HttpRequestClass;
@@ -7,29 +8,61 @@ import org.wikiRelationsHeadMaster.core.networking.HttpRequestClass;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class RawLinksHandler {
-    public ArrayList<QueryThread> queryThreadList;
     String url;
+    ArrayList<QueryThread> queryThreadArrayList;
 
-    public RawLinksHandler(ArrayList<QueryThread> queryThreadList) {
-        this.queryThreadList = queryThreadList;
-
+    public RawLinksHandler(String url, ArrayList<QueryThread> queryThreadArrayList) {
+        this.url = url;
+        this.queryThreadArrayList = queryThreadArrayList;
     }
 
-//    public ArrayList<LinksObject> getListObjectFromQueryThreads() {
-//        for (QueryThread queryThread: queryThreadList) {
-//
-//        }
-//    }
+    public ArrayList<LinksObject> getListObjectFromQueryThreads(ArrayList<QueryThread> queryThreadList) {
+        ArrayList<LinksObject> linksObjectArrayList = new ArrayList<>();
+        for (QueryThread queryThread: queryThreadList) {
+            if (queryThread.isReadyToBeSent()){
+            linksObjectArrayList.add(queryThread.getLinksObject());
+        }}
 
-    public void addLinksToSend(QueryThread queryThread) {
-        queryThreadList.add(queryThread);
+        return linksObjectArrayList;
+    }
 
+    public void sendAllRawReadyLinks(ArrayList<QueryThread> queryThreadsList) throws IOException, InterruptedException {
+        ArrayList<LinksObject> linksObjects = new ArrayList<>();
+        for (QueryThread queryThread : queryThreadsList) {
+            if (queryThread.isReadyToBeSent() && !queryThread.isLinksSent())
+                linksObjects.add(queryThread.getLinksObject());
+                queryThread.setLinksSent(true);
+        }
+        sendLinks(linksObjects);
+        if (!linksObjects.isEmpty()) {
+            System.out.println("Sent raw links");
+        }
+    }
+
+
+    public Hashtable<Integer, Hashtable<String, Object>> getRankedLinksAndParse() throws IOException {
+        String getResponse = HttpRequestClass.sendGETRequest(this.url);
+        return JsonParsingClass.parseLinks(getResponse);
+    }
+
+    public ArrayList<QueryThread> updateAllQueryThreads(Hashtable<Integer, Hashtable<String, Object>> linksHash) {
+        for (QueryThread queryThread: this.queryThreadArrayList) {
+            if (linksHash.get(queryThread.getID()) != null) {
+                Hashtable<String, Object> linkhash = linksHash.get(queryThread.getID());
+                queryThread.setOriginalLink((String) linkhash.get("Original Link"));
+                queryThread.setLinksObjectLinks((ArrayList<String>) linkhash.get("Links"));
+                queryThread.setReadyToSendCircularLinks(true);
+            }
+        }
+        return this.queryThreadArrayList;
     }
 
     public void sendLinks(ArrayList<LinksObject> linksObjects) throws IOException, InterruptedException {
         ArrayList<HashMap<String, Object>> hashMapArrayList =  HttpRequestClass.turnLinksArrayToHashmap(linksObjects);
-        HttpRequestClass.sendPOSTRequest(hashMapArrayList, this.url);
-    }
+        for (HashMap<String, Object> hashMap: hashMapArrayList) {
+            HttpRequestClass.sendPOSTRequest(hashMap, this.url);
+    }}
 }
