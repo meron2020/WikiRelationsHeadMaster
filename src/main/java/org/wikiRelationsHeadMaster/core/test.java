300b88294af65312e407bb6b80a9f69d2768f5f2
Yoav <yoav.meron11@gmail.com>
2021-12-24 19:37:39 +0200

    Tested ApplicationManager

--- src/main/java/org/wikiRelationsHeadMaster/core/test.java
+++ src/main/java/org/wikiRelationsHeadMaster/core/test.java
@@ -1,18 +1,44 @@
 package org.wikiRelationsHeadMaster.core;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.wikiRelationsHeadMaster.core.Parsers.HtmlParser;
+import org.wikiRelationsHeadMaster.core.Parsers.JsonParsingClass;
+import org.wikiRelationsHeadMaster.core.networking.HttpRequestClass;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class test extends Thread {
 
-    public void run(int num) throws InterruptedException {
+    public void run(int num) {
 
     }
-    public static void main(String[] args) throws InterruptedException {
-        int num = 0;
 
-        test Test = new test();
-        Test.start(num);
-
-        num = 1;
+    public void addString(String string, ArrayList<String> arrayList) {
+        arrayList.add(string);
     }
+
+    public static void main(String[] args) throws InterruptedException, IOException
+    {
+        ArrayList<HashMap<String, String >> topTenRanked;
+        ApplicationManager applicationManager = new ApplicationManager();
+//        applicationManager.startApplication();
+        applicationManager.addThread("Israel", 20);
+        while (true) {
+            ArrayList<HashMap<String, String >> ranked = applicationManager.getRankedLinksById(20);
+            if (!ranked.isEmpty()) {
+                topTenRanked = ranked;
+                break;
+            }
+            Thread.sleep(15000);
+        }
+        System.out.println(topTenRanked);
+
+    }
+
 
     private void start(int num) throws InterruptedException {
         while (true) {
