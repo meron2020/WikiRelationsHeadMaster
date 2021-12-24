3155532a00fa76e27d8005ee4c36ca902ed290d3
Yoav <yoav.meron11@gmail.com>
2021-12-24 19:36:33 +0200

    Added all necessary parts and resources for application to run.

--- src/main/java/org/wikiRelationsHeadMaster/wikiRelationsHeadMasterApplication.java
+++ src/main/java/org/wikiRelationsHeadMaster/wikiRelationsHeadMasterApplication.java
@@ -3,6 +3,11 @@ package org.wikiRelationsHeadMaster;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.wikiRelationsHeadMaster.core.ApplicationManager;
+import org.wikiRelationsHeadMaster.resources.FrontPageResource;
+import org.wikiRelationsHeadMaster.resources.WikiRelationsResource;
+
+import java.util.ArrayList;
 
 public class wikiRelationsHeadMasterApplication extends Application<wikiRelationsHeadMasterConfiguration> {
 
@@ -24,6 +29,11 @@ public class wikiRelationsHeadMasterApplication extends Application<wikiRelation
     public void run(final wikiRelationsHeadMasterConfiguration configuration,
                     final Environment environment) {
         // TODO: implement application
+        ArrayList<Integer> idArrayList = new ArrayList<>();
+        ApplicationManager applicationManager = new ApplicationManager();
+        new Thread(applicationManager).start();
+        environment.jersey().register(new WikiRelationsResource(applicationManager, idArrayList));
+        environment.jersey().register(new FrontPageResource(idArrayList));
     }
 
 }
