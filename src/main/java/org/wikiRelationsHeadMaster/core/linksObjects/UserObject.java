8d7e9e032eeed34bb35aae4d59424fad5ac25a6e
Yoav <yoav.meron11@gmail.com>
2021-12-24 19:36:55 +0200

    Created UserObject to keep track of user ids and queries.

---
+++ src/main/java/org/wikiRelationsHeadMaster/core/linksObjects/UserObject.java
@@ -0,0 +1,26 @@
+package org.wikiRelationsHeadMaster.core.linksObjects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserObject {
+    @JsonProperty("id")
+    Integer id;
+    @JsonProperty("query")
+    String query;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
