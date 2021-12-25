b1acf27067b287cc08504526cbb0d38f8adee845
Yoav <yoav.meron11@gmail.com>
2021-12-25 09:48:17 +0200

    Added new checks for links in HtmlParser

--- src/main/java/org/wikiRelationsHeadMaster/core/Parsers/HtmlParser.java
+++ src/main/java/org/wikiRelationsHeadMaster/core/Parsers/HtmlParser.java
@@ -6,17 +6,29 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HtmlParser {
-    public static ArrayList<String> findAllWikiLinks(Document document) {
+    public static ArrayList<String> findAllWikiLinks(Document document) throws InterruptedException {
         ArrayList<String> wikiLinks = new ArrayList<>();
         Elements aElements = document.select("a");
-        for (Element element: aElements) {
-            String link = element.attr("href");
-            if (link.contains("https://en.wikipedia.org/wiki/")) {
-                wikiLinks.add(link);
+        for (Element aElement : aElements) {
+            String link = aElement.attr("href");
+            String[] linkParts = link.split("/");
+            try {
+                if (Objects.equals(linkParts[3], "wiki")) {
+                    if (!link.contains(":") && !link.contains("%")) {
+                        if (!wikiLinks.contains("https://en.wikipedia.org" + link)) {
+                            wikiLinks.add("https:" + link);
+                            System.out.println(link);
+                }}}
+            } catch (Exception ignored) {
             }
+
         }
+
+        System.out.println(wikiLinks.size());
+
         return wikiLinks;
-    }
+}
 }
